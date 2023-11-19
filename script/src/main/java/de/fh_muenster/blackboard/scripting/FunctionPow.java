a5a1c68f6ad5d6a5893962a4d007ff09cd968cd4
Djordy von Rönn <vonronn@icloud.com>
2023-11-19 14:37:35 +0100

    FunctionPow implemented

---
+++ script/src/main/java/de/fh_muenster/blackboard/scripting/FunctionPow.java
@@ -0,0 +1,48 @@
+package de.fh_muenster.blackboard.scripting;
+
+import java.util.function.Function;
+
+public class FunctionPow implements Function<double [], Double> {
+
+    private FunctionNode function;
+
+    private Function<double [], Double> left;
+
+    private Function<double [], Double> right;
+
+    public FunctionPow(Function<double[], Double> left, Function<double[], Double> right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public FunctionNode getFunction() {
+        return function;
+    }
+
+    public void setFunction(FunctionNode function) {
+        this.function = function;
+    }
+
+    @Override
+    public Double apply(double[] doubles) {
+        double leftSide = left.apply(doubles);
+        double rightSide = right.apply(doubles);
+        if (leftSide == 0 && rightSide < 0) {
+            throw new IllegalArgumentException("Wir teilen durch 0 bei pow!");
+        }
+        if (leftSide < 0 && (rightSide % 1) != 0) {
+            throw new IllegalArgumentException("Complex number bei pow!");
+        }
+        return Math.pow(leftSide, rightSide);
+    }
+
+    @Override
+    public <V> Function<V, Double> compose(Function<? super V, ? extends double[]> before) {
+        return Function.super.compose(before);
+    }
+
+    @Override
+    public <V> Function<double[], V> andThen(Function<? super Double, ? extends V> after) {
+        return Function.super.andThen(after);
+    }
+}
