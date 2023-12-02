aa8e7c075b9e67b7f98e9fca2a28a0615a73a39c
thomas <tf670992@fh-muenster.de>
2023-12-02 06:33:05 +0100

    Vector matrix multiplication.

--- script/src/test/java/de/fh_muenster/blackboard/scripting/LinearAlgebraTest.java
+++ script/src/test/java/de/fh_muenster/blackboard/scripting/LinearAlgebraTest.java
@@ -3,8 +3,6 @@ package de.fh_muenster.blackboard.scripting;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
