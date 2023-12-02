f50c54ca529e515ef8e2b7959dbd4b165ca261d0
Djordy von Rönn <vonronn@icloud.com>
2023-12-02 15:31:49 +0100

    Thomas weiß es besser

--- script/src/main/java/de/fh_muenster/blackboard/scripting/DerivativeVisitor.java
+++ script/src/main/java/de/fh_muenster/blackboard/scripting/DerivativeVisitor.java
@@ -27,7 +27,6 @@ import java.util.function.Function;
  *	A value visitor for ast.
  */
 public class DerivativeVisitor{
-	FunctionVisitor functionVisitor = new FunctionVisitor();
 
 	static String variable;
 	public Function<double [], Double> visit(AbstractFunction toVisit){
@@ -55,109 +54,72 @@ public class DerivativeVisitor{
 		return new FunctionDoubleValue(0.0);
 	}
 
-	/**
-	 * (non-Javadoc)
-	 *
-	 * @see AstVisitor#visit(OperationNode)
-	 */
-	public Function<double[], Double> visit(OperationNode n) {
-		Function<double[], Double> lsDerivative = visit(n.left());
-		Function<double[], Double> rsDerivative = visit(n.right());
-		Function<double[], Double> ls = n.left().accept(functionVisitor);
-		Function<double[], Double> rs = n.right().accept(functionVisitor);
-		Operation op = n.data();
+	public Function<double[], Double> visit(AbstractFunctionTwoVariable n) {
+		Function<double[], Double> lsDerivative = visit(n.getLeft());
+		Function<double[], Double> rsDerivative = visit(n.getRight());
+		Function<double[], Double> ls = n.getLeft();
+		Function<double[], Double> rs = n.getRight();
+		String op = n.toString();
 
-		switch (op) {
-			case PLUS:
-				return new FunctionPlus(lsDerivative, rsDerivative);
-			case MINUS:
-				return new FunctionMinusBinary(lsDerivative, rsDerivative);
-			case TIMES:
-				return new FunctionPlus(new FunctionTimes(lsDerivative, rs), new FunctionTimes(ls, rsDerivative));
-			case DIVIDE:
-				return new FunctionDivide(new FunctionMinusBinary(new FunctionTimes(lsDerivative, rs), new FunctionTimes(ls, rsDerivative)), new FunctionPow(rs, new FunctionDoubleValue(2.0)));
-			case POWER, POWERCARET:
+		if (n instanceof FunctionTimes) {
+			return new FunctionPlus(new FunctionTimes(lsDerivative, rs), new FunctionTimes(ls, rsDerivative));
+		} else if (n instanceof FunctionPlus) {
+			return new FunctionPlus(lsDerivative, rsDerivative);
+		} else if (n instanceof FunctionMinusBinary) {
+			return new FunctionMinusBinary(lsDerivative, rsDerivative);
+		} else if (n instanceof FunctionDivide) {
+			return new FunctionDivide(new FunctionMinusBinary(new FunctionTimes(lsDerivative, rs), new FunctionTimes(ls, rsDerivative)), new FunctionPow(rs, new FunctionDoubleValue(2.0)));
+		} else if (n instanceof FunctionPow) {
+			Function<double[], Double> f = n.getLeft();
+			Function<double[], Double> g = n.getRight();
 
-				Function<double[], Double> f = n.childs().get(0).accept(functionVisitor);
-				Function<double[], Double> g = n.childs().get(1).accept(functionVisitor);
-
-				Function<double[], Double> fDerivative = visit(n.childs().get(0));
-				Function<double[], Double> gDerivative = visit(n.childs().get(1));
-				return new FunctionTimes(new FunctionPow(f,g),new FunctionPlus(new FunctionDivide(new FunctionTimes(g, fDerivative), f), new FunctionTimes(gDerivative, new FunctionLog(f))));
+			Function<double[], Double> fDerivative = visit(n.getLeft());
+			Function<double[], Double> gDerivative = visit(n.getRight());
+			return new FunctionTimes(new FunctionPow(f,g),new FunctionPlus(new FunctionDivide(new FunctionTimes(g, fDerivative), f), new FunctionTimes(gDerivative, new FunctionLog(f))));
+		} else {
+			throw new IllegalArgumentException("unkown operation: " + op);
 		}
-		throw new IllegalArgumentException("unkown operation: " + op);
-	}
-
-	/**
-	 * (non-Javadoc)
-	 *
-	 * @see AstVisitor#visit(AssignNode)
-	 */
-	public Function<double[], Double> visit(AssignNode n) {
-		return visit(n.right());
 	}
 
 	public Function<double[], Double> visit(AbstractFunctionOneVariable n) {
 		Function<double[], Double> functionVariableDerivative = visit(n.getChild());
+
 		if (n instanceof FunctionMinusUnary) {
-			return new FunctionMinusUnary(visit(n.getChild()));
+			return new FunctionMinusUnary(functionVariableDerivative);
+
 		} else if (n instanceof FunctionPlusUnary) {
-			return new FunctionPlusUnary(visit(n.getChild()));
+			return new FunctionPlusUnary(functionVariableDerivative);
+
 		} else if (n instanceof FunctionCos) {
 			return new FunctionTimes(new FunctionMinusUnary(new FunctionSin(n.getChild())), functionVariableDerivative);
+
 		} else if (n instanceof FunctionSin) {
+			return new FunctionTimes(new FunctionCos(n.getChild()), functionVariableDerivative);
 
+		} else if (n instanceof FunctionAcos) {
+			return new FunctionTimes(new FunctionMinusUnary(new FunctionPow(new FunctionMinusBinary(new FunctionDoubleValue(1.0), new FunctionPow(visit(n.getChild()),
+					new FunctionDoubleValue(2.0))), new FunctionMinusUnary(new FunctionDoubleValue(0.5)))), functionVariableDerivative);
+
+		} else if (n instanceof FunctionAsin) {
+			return new FunctionTimes(new FunctionDivide(new FunctionDoubleValue(1.0), new FunctionPow(new FunctionMinusBinary(new FunctionDoubleValue(1.0),
+					new FunctionPow(n.getChild(), new FunctionDoubleValue(2.0))), new FunctionDoubleValue(0.5))), functionVariableDerivative);
+
+		} else if (n instanceof FunctionLog) {
+			return new FunctionTimes(new FunctionDivide(new FunctionDoubleValue(1.0), n.getChild()), functionVariableDerivative);
+
+		} else if (n instanceof FunctionLogB) {
+			return new FunctionTimes(new FunctionDivide(new FunctionDoubleValue(1.0), new FunctionTimes(visit(n.getChild()),
+					new FunctionLog(new FunctionDoubleValue(2.0)))), functionVariableDerivative);
+
+		} else if (n instanceof FunctionExp) {
+			return new FunctionTimes(new FunctionExp(n.getChild()), functionVariableDerivative);
+		} else {
+			throw new IllegalArgumentException("unknown operation: " + n.toString());
 		}
-
-		throw new IllegalArgumentException("unknown operation: " + n.data());
 	}
 
-
-
-
-	public Function<double[], Double> visit(FunctionNode n) {
-		Function<double[], Double> functionVariableDerivative = visit(n.childs().get(0));
-		if(n.data().equals("lb")){
-			return new FunctionTimes(new FunctionDivide(new FunctionDoubleValue(1.0), new FunctionTimes(visit(n.childs().get(0)), new FunctionLog(new FunctionDoubleValue(2.0)))), functionVariableDerivative);
-
-		}
-		if(n.data().equals("ln")){
-			return new FunctionTimes(new FunctionDivide(new FunctionDoubleValue(1.0), n.childs().get(0).accept(functionVisitor)), functionVariableDerivative);
-		}
-
-		if(n.data().equals("pow")){
-			Function<double[], Double> f = n.childs().get(0).childs().get(0).accept(functionVisitor);
-			Function<double[], Double> g = n.childs().get(0).childs().get(1).accept(functionVisitor);
-
-			Function<double[], Double> fDerivative = visit(n.childs().get(0).childs().get(0));
-			Function<double[], Double> gDerivative = visit(n.childs().get(0).childs().get(1));
-
-			return new FunctionTimes(new FunctionPow(f,g),new FunctionPlus(new FunctionDivide(new FunctionTimes(g, fDerivative), f), new FunctionTimes(gDerivative, new FunctionLog(f))));
-		}
-		if(n.data().equals("sin")){
-			return new FunctionTimes(new FunctionCos(n.childs().get(0).accept(functionVisitor)), functionVariableDerivative);
-		}
-		if(n.data().equals("cos")){
-			return new FunctionTimes(new FunctionMinusUnary(new FunctionSin(n.childs().get(0).accept(functionVisitor))), functionVariableDerivative);
-		}
-		if(n.data().equals("acos")) { // −(1 − x^2)^(−1/2)
-			return new FunctionTimes(new FunctionMinusUnary(new FunctionPow(new FunctionMinusBinary(new FunctionDoubleValue(1.0), new FunctionPow(visit(n.childs().get(0)),
-					new FunctionDoubleValue(2.0))), new FunctionMinusUnary(new FunctionDoubleValue(0.5)))), functionVariableDerivative);
-		}
-		if(n.data().equals("asin")){ // 1/(1-(x^2))
-			return new FunctionTimes(new FunctionDivide(new FunctionDoubleValue(1.0), new FunctionPow(new FunctionMinusBinary(new FunctionDoubleValue(1.0),
-					new FunctionPow(n.childs().get(0).accept(functionVisitor), new FunctionDoubleValue(2.0))), new FunctionDoubleValue(0.5))),
-					visit(n.childs().get(0)));
-		}
-		if(n.data().equals("exp")){
-			return new FunctionTimes(new FunctionExp(n.childs().get(0).accept(functionVisitor)), functionVariableDerivative);
-		}
-		AST<?> function = FunctionMap.functions.get(n.data());
-		if(function.parent() instanceof FunctionAssignNode){
-			function = ((FunctionAssignNode) function.parent()).expr();
-		}
-		n.setFunctionCall(function.accept(functionVisitor));
-		return new FunctionTimes(visit(function), functionVariableDerivative);
+	public Function<double[], Double> visit(FunctionAssignNode functionAssignNode) {
+		return null;
 	}
 
 	/**
