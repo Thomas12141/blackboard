ff021ca681afe2065ee07811c58b72e0ce9274d9
Djordy von Rönn <vonronn@icloud.com>
2024-01-10 17:11:47 +0100

    new

---
+++ analysis/src/test/java/de/fh/muenster/blackboard/analysis/AbstractDifferentiatorTesting.java
@@ -0,0 +1,81 @@
+/*
+ * Project: analysis
+ *
+ * Copyright (c) 2023,  Prof. Dr. Nikolaus Wulff
+ * University of Applied Sciences, Muenster, Germany
+ * Lab for computer sciences (Lab4Inf).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package de.fh.muenster.blackboard.analysis;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static java.lang.Math.*;
+import java.util.function.Function;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Abstract base test of the Differentiator.
+ * @author nwulff
+ * @since  08.12.2023
+ *
+ */
+abstract class AbstractDifferentiatorTesting {
+	double delta = 5.E-8;
+	Differentiator differentiator;
+	@BeforeEach
+	void setUp() {
+		differentiator = createDifferentiator();
+	}
+	/**
+	 * Factory method to create native or java Differentiator.
+	 * @return
+	 */
+	protected abstract Differentiator createDifferentiator();
+	/**
+	 * Test method for {@link de.fh.muenster.blackboard.analysis.Differentiator#differential(java.util.function.Function)}.
+	 */
+	@Test
+	void testDifferential() {
+		double[] arg = new double[1];
+		Function<double[],Double> f = (x)-> pow(x[0],3);
+		Function<double[],Double> expected = (x)-> 3*pow(x[0],2);
+		Function<double[],Double> returned = differentiator.differential(f);
+		double expect,retur;
+        for(int j=0;j<20;j++) {
+        	arg[0] = 2*(Math.random()-0.5);
+        	expect = expected.apply(arg);
+        	retur  = returned.apply(arg);
+        	assertEquals(expect,retur,delta);
+        }
+	}
+	/**
+	 * Test method for {@link de.fh.muenster.blackboard.analysis.Differentiator#differentiate(java.util.function.Function, double[])}.
+	 */
+	@Test
+	void testDifferentiate() {
+		double[] arg = new double[1];
+		Function<double[],Double> f = (x)-> sin(x[0]);
+		Function<double[],Double> df = (x)-> cos(x[0]);
+		double expected,returned;
+        for(int j=0;j<20;j++) {
+        	arg[0] = 2*(Math.random()-0.5);
+        	expected = df.apply(arg);
+        	returned = differentiator.differentiate(f,arg,delta);
+        	assertEquals(expected,returned,delta);
+        }
+	}
+}
