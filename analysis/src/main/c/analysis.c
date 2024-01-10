b13e2b0690c6fd379e9d8130b1ab52515f00f2d7
thomas <tf670992@fh-muenster.de>
2024-01-10 19:21:30 +0100

    test

--- analysis/src/main/c/analysis.c
+++ analysis/src/main/c/analysis.c
@@ -83,6 +83,80 @@ double xHoch2(double x){
 char* errors(){
     return errorsArr;
 }
+
+/*int main() {
+    double derivative = differentiate(xHoch2, 0.0, 0.000000001);
+    printf("%lf\n", derivative); //Muss 0 sein
+
+    derivative = differentiate(xHoch2, 3.0, 0.000000001);
+    printf("%lf\n", derivative); //Muss 6 sein
+
+    double integral = integrate(xHoch2, 0, 3.0, 0.000000001);
+    printf("%lf\n", integral); //Muss 9 sein
+
+    double sinIntegral = integrate(sin, 0, M_PI, 0.0000001);
+    printf("Numerisches Integral von sin(x) von 0 bis PI: %lf\n", sinIntegral); // Muss 2 sein
+
+    double expDerivative = differentiate(exp, 1.0, 0.000000001);
+    printf("Numerische Ableitung an x=1.0 von exp(x): %lf\n", expDerivative); // Muss exp(1.0) sein
+
+    double logDerivative = differentiate(log, 2.0, 0.000000001);
+    printf("Numerische Ableitung an x=2.0 von log(x): %lf\n", logDerivative); // Muss 0.5 sein
+
+    double sqrtDerivative = differentiate(sqrt, 4.0, 0.000000001);
+    printf("Numerische Ableitung an x=4.0 von sqrt(x): %lf\n", sqrtDerivative); // Muss 0.25 sein
+
+    double cosIntegral = integrate(cos, 0, M_PI, 0.0000001);
+    printf("Numerisches Integral von cos(x) von 0 bis PI: %lf\n", cosIntegral); // Muss 0 sein
+
+    double sinhDerivative = differentiate(sinh, 0.5, 0.000000001);
+    printf("Numerische Ableitung an x=0.5 von sinh(x): %lf\n", sinhDerivative); // Muss cosh(0.5) sein
+
+    double coshIntegral = integrate(cosh, 0, 1.0, 0.0000001);
+    printf("Numerisches Integral von cosh(x) von 0 bis 1.0: %lf\n", coshIntegral); // Muss sinh(1.0) sein
+
+    double powDerivative = differentiate(powerFunction, 2.0, 0.000000001);
+    printf("Numerische Ableitung an x=2.0 von pow(x,2): %lf\n", powDerivative); // Muss 4.0 sein
+
+    double powIntegral = integrate(powerFunction, 0, 2.0, 0.0000001);
+    printf("Numerisches Integral von pow(x,2) von 0 bis 2.0: %lf\n", powIntegral); // Muss 8/3 sein
+
+    double sinExpIntegral = integrate(sinPlusExp, 0, M_PI, 0.0000001);
+    printf("Numerisches Integral von sin(x) + exp(x) von 0 bis PI: %lf\n", sinExpIntegral);
+
+    double logSqrtIntegral = integrate(logPlusSqrt, 1.0, 4.0, 0.0000001);
+    printf("Numerisches Integral von log(x) + sqrt(x) von 1.0 bis 4.0: %lf\n", logSqrtIntegral);
+
+    // Test mit der Funktion x^3 von 0 bis 2
+    double integralXCubed = integrate(xCubed, 0, 2, 0.0000001);
+    printf("Numerisches Integral von x^3 von 0 bis 2: %lf\n", integralXCubed);
+
+    // Test mit der Funktion e^{-x} von 0 bis 1
+    double integralExpNegX = integrate(expNegX, 0, 1, 0.0000001);
+    printf("Numerisches Integral von e^{-x} von 0 bis 1: %lf\n", integralExpNegX);
+
+    // Test mit der Funktion cos(x) von 0 bis PI
+    double integralCosX = integrate(cosX, 0, M_PI, 0.0000001);
+    printf("Numerisches Integral von cos(x) von 0 bis PI: %lf\n", integralCosX);
+
+    // Test mit der Funktion sin(x) von 0 bis PI
+    double integralSinX = integrate(sinX, 0, M_PI, 0.0000001);
+    printf("Numerisches Integral von sin(x) von 0 bis PI: %lf\n", integralSinX);
+
+    // Test mit der Funktion sqrt(x) von 1 bis 4
+    double integralSqrtX = integrate(sqrtX, 1, 4, 0.0000001);
+    printf("Numerisches Integral von sqrt(x) von 1 bis 4: %lf\n", integralSqrtX);
+
+
+    if (strlen(errorsArr) > 0) {
+        printf("\nFehler: %s\n", errorsArr);
+    } else {
+        printf("\nKeine Fehler!");
+    }
+
+
+    return 0;
+}*/
 #ifdef __cplusplus
 }
 #endif
